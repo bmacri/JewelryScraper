c835960ca8d4022c6d87f0382d06583dc3865ab6
CodePhoenix <bethany.macri@gmail.com>
2012-04-21 18:58:43 -0700

    Parsing the first link out of an Anthropologie product list

--- one_site_scraper.py
+++ one_site_scraper.py
@@ -1,9 +1,33 @@
 from datetime import date
 import requests
+import string
+
+def get_urls(websites_file,filepath):
+    #reads urls in the websites.txt one by one, adding to a url list and then creating a filename using generate_filename
+    f = open(websites_file,'r')
+    url_list = []
+    for url in f:
+        url_list.append(url)
+    for url in url_list:
+        generate_filename(url)
+    return
 
 
+def get_contents(url):
+    #retrieves the contents of the page using request and assigns the contents to the variable contents
+    r = requests.get(url)
+    contents = r.text
+    return contents
+
+def generate_filename(url,file_date=None):
+    #generates a filename which will appear as 'date-url'
+    if file_date == None:
+        file_date = date.today()
+    filename = str(file_date) + '-' + url
+    return filename
 
 def save_page(url,filepath):
+    #writes the contents of a url to file created with generate file and saves it under the filepath given
     if not url.startswith('http'):
         raise Exception('url must start with http')
     contents = get_contents(url)
@@ -14,47 +38,64 @@ def save_page(url,filepath):
     f.close()
     return 
 
-def get_contents(url):
-    r = request.get(url)
-    contents = r.text
+def file_contents(filename):
+    f = open(filename, 'r')
+    contents = f.read()
+    f.close()
     return contents
 
-def get_urls(websites_file,filepath): 
-    f = open(websites_file,'r')
-    url_list = []
-    for url in f:
-        url_list.append(line)
-    for url in url_list:
-        save_page(url,filepath)
-    return
+#f = open(filename,'r') #should i call this contents instead of f?
+
+def trim_contents(contents):
+    begin_index = contents.find('<div class="category-item"')
+    end_index = contents.find('<a name="pagelast" id="pagelast" href=""></a>')
+    trimmed_contents = contents[begin_index:end_index] 
+    return trimmed_contents
+
+def anthro_product_urls(contents):
+    anthro_product_url_list = []
+    trimmed_contents = trim_contents(contents)   
+    start_link = trimmed_contents.find('<a href=')
+    if start_link == -1:
+        return anthro_product_url_list
+    start_quote = trimmed_contents.find('"',start_link)
+    end_quote = trimmed_contents.find('"', start_quote + 1)
+    next_url = trimmed_contents[start_quote + 1:end_quote]
+    anthro_product_url_list.append(next_url)
+    #anthro_product_urls(trimmed_contents[end_quote+1:])
+    return anthro_product_url_list
+       
+def get_price(filename):
+    product_dict = {}
+    trim_contents(filename)
+    dict_val = []
+    for url in anthro_product_urls:
+        begin_price_index = contents.find('$',url)
+        end_price_index = contents.find('<', begin_price_index + 1)
+        price = contents[begin_price_index:end_price_index]
+        dict_val.append(price)
+        product_dict[url] = dict_val
+    return product_dict
+
+"""def get_image(filename):
+    trim_contents(filename)
+    for url in anthro_product_urls:
+        begin_img_index = contents.find('image src',url)
+        end_img_index = contents.find('"', begin_img_index + 1)
+        image = contents[begin_img_index:end_img_index]
+        dict_val.append(image)
+        product_dict[url] = dict_val
+    return product_dict"""
+
+        
+
+contents = file_contents('/home/bethany/Jewelry_Crawler/jewelryaccessories-shopjewelry.jsp')
+assert anthro_product_urls(contents)[0] == "/anthro/product/jewelryaccessories-shopjewelry/23918493.jsp", anthro_product_urls(contents)[0]
+assert trim_contents(contents)[:27] == '<div class="category-item">'
 
 
-class AnthroParser:
-    def next_level_urls(self,contents):
-        pass
-
-
-
-    def product_urls(self,contents):
-        pass
-
-f = open('jewelryaccessories-shopjewelry.jsp','r')
-ap = AnthroParser()
-urls = ap.product_urls(f.read())
-
-assert urls[0] == 'http://www.anthropologie.com/anthro/product/jewelryaccessories-shopjewelry/23918493.jsp', urls[0]
-
-def generate_filename(url,file_date=None):
-    if file_date == None:
-        file_date = date.today()
-    filename = str(file_date) + '-' + url
-    return filename
-
-assert generate_filename('www.google.com') == '2012-04-15-www.google.com',generate_filename('www.google.com')
+#assert generate_filename('www.google.com') == '2012-04-15-www.google.com',generate_filename('www.google.com')
  
 
-#https://gist.github.com/973705
-
-get_urls('websites.txt','/home/bethany/Jewelry_Crawler')
+#get_urls('websites.txt','/home/bethany/Jewelry_Crawler')
     
-
