043d2e7e71ed1380170bd3ac2c44fe65143d37e3
CodePhoenix <bethany.macri@gmail.com>
2012-05-01 20:34:57 -0700

    Removed anthro.py and replaced it with anthroparser.py.  Moved more functions from anthro.py into superparser.py (which holds SuperParser, the super class for all of the retail parser classes).  Updated everything to OOP.

--- anthroparser.py
+++ anthroparser.py
@@ -1,7 +1,7 @@
 from superparser import SuperParser
 import settings
-
-
+import re
+from datetime import date
 
 class AnthroParser(SuperParser):
     def __init__(self):
@@ -28,10 +28,107 @@ class AnthroParser(SuperParser):
         anthro_product_url_list.extend(rest_of_urls)
         return anthro_product_url_list
 
+
+    def get_product_name(self,contents):
+        start_meta = contents.find('<meta property="og:title" content=')
+        start_og = contents.find('"', start_meta + 1)
+        end_og = contents.find('"', start_og + 1)
+        start_quote = contents.find('"', end_og + 1)
+        end_quote = contents.find('"', start_quote + 1)
+        product_name = contents[start_quote + 1:end_quote]
+        return product_name
+
+    def ignore_also_like(self,contents):
+        also_like = contents.find('you may also like')
+        contents = contents[:also_like]
+        return contents
+        
+    def get_product_id(self,contents):
+        contents = self.ignore_also_like(contents)
+        find_canonical = contents.find('<link rel="canonical"')
+        contents = contents[find_canonical:]
+        match_product_id = re.search(r"[A-Z]?[0-9]+",contents)
+        if match_product_id:
+            product_id = match_product_id.group()
+        else:
+            return None
+        return product_id
+
+    def get_price(self,contents):
+        contents = self.ignore_also_like(contents)
+        match_get_price = re.search(r"\$[0-9]+\.[0-9][0-9]",contents)
+        if match_get_price:
+            price = match_get_price.group()
+        else:
+            return None
+        return price
+
+    def get_image(self,contents):
+        contents = self.ignore_also_like(contents)
+        find_img_tag = contents.find('<img')
+        start_img_url = contents.find("src='", find_img_tag)
+        end_img_url = contents.find("'",start_img_url+5)
+        image = contents[start_img_url+5:end_img_url]
+        return image
+
+    def get_description(self,contents):
+        contents = self.ignore_also_like(contents)
+        desc_element = contents.find('<div id="productDescription">')
+        details_index = contents.find('DETAILS',desc_element)
+        contents = contents[details_index+7:]
+        non_angle = re.split(r"[<>]",contents)
+        description = []
+        i=0
+        while i<len(non_angle):
+            if i % 2 == 0:
+                description.append(non_angle[i].strip('\n '))
+            i = i + 1
+        description = filter(None, description)
+        description = " ".join(description)
+        return description
+
+#------------------------------------------------------------------------------------------------------
+
 anthro = AnthroParser()
 
 contents = anthro.file_contents(settings.project_path + 'anthro_alljewelry.jsp')
-assert anthro.get_product_urls(contents)[0] == "http://www.anthropologie.com/anthro/product/jewelryaccessories-shopjewelry/23918493.jsp", anthro.get_product_urls(contents)[0]
-assert anthro.get_product_urls(contents)[1] == "http://www.anthropologie.com/anthro/product/jewelryaccessories-shopjewelry/A23918493.jsp", anthro.get_product_urls(contents)[1]
-assert anthro.get_product_urls(contents)[-1] == "http://www.anthropologie.com/anthro/product/jewelryaccessories-shopjewelry/24111676.jsp", anthro.get_product_urls(contents)[-1]
-assert anthro.trim_contents(contents)[:27] == '<div class="category-item">'
+anthro_url_list = anthro.get_product_urls(contents)
+assert anthro_url_list[0] == "http://www.anthropologie.com/anthro/product/jewelryaccessories-shopjewelry/23918493.jsp", anthro_url_list[0]
+assert anthro_url_list[1] == "http://www.anthropologie.com/anthro/product/jewelryaccessories-shopjewelry/A23918493.jsp", anthro_url_list[1]
+assert anthro_url_list[-1] == "http://www.anthropologie.com/anthro/product/jewelryaccessories-shopjewelry/24111676.jsp", anthro_url_list[-1]
+assert anthro.trim_contents(contents)[:27] == '<div class="category-item">', anthro.trim_contents(contents)[:27]
+
+contents = anthro.file_contents(settings.project_path + 'stacked_stone.html')
+product = anthro.product_details(contents)
+assert product['name'] == "Stacked Stone Drops", product['name']
+
+assert anthro.get_price(contents) == "$158.00", anthro.get_price(contents)
+
+assert anthro.get_image(contents) == 'http://images.anthropologie.com/is/image/Anthropologie/23918493_040_b?$product410x615$', anthro.get_image(contents)
+
+assert anthro.get_description(contents) == 'Slabs of turquoise or jade dangle faceted quartz orbs. By Sura Jewelry. Quartz, 24k gold plated bronze, jade 1.5"L, 0.75"W Turkey', anthro.get_description(contents) 
+
+contents = anthro.file_contents('prod_id_with_letter.html')
+assert anthro.get_product_id(contents) == 'A23918493', anthro.get_product_id(contents)
+
+test_date = date(2012,4,15)
+assert anthro.generate_filename('www.google.com',file_date=test_date) == '2012-04-15-www.google.com',anthro.generate_filename('www.google.com')
+
+assert anthro.get_description(contents) == 'Slabs of turquoise or jade dangle faceted quartz orbs. By Sura Jewelry. Quartz, 24k gold plated bronze, turquoise 1.5"L, 0.75"W Turkey', anthro.get_description(contents)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- superparser.py
+++ superparser.py
@@ -1,6 +1,36 @@
+from datetime import date
+import requests
+import MySQLdb
+import settings 
+
 class SuperParser:
     def __init__(self):
         pass
+
+    def get_contents(self,url):
+    #goes to a webpage, and saved the html on that page to the variable contents
+        r = requests.get(url)
+        contents = r.text
+        return contents
+
+    def generate_filename(self,url,file_date=None):
+        "generates a filename which will appear as 'date-url'"
+        if file_date == None:
+            file_date = date.today()
+        filename = str(file_date) + '-' + url
+        return filename
+
+    def save_page(self,url,filepath):
+        #writes the contents of a url to file created with generate file and saves it under the filepath given
+        if not url.startswith('http'):
+            raise Exception('url must start with http')
+        contents = get_contents(url)
+        filename = generate_filename(url)
+        filename = filename.replace('/','_')
+        f = open(filepath + '/' + filename,'w')
+        f.write(contents)
+        f.close()
+        return 
 
     def file_contents(self,filename):
         #opens a file (NOT a webpage, as in get_contents), reads the text of the file, assigns it to variable contents and returns contents
@@ -8,3 +38,24 @@ class SuperParser:
         contents = f.read()
         f.close()
         return contents
+
+    def product_details(self,contents):
+        "takes the contents of a file and creates a dictionary with the string 'name' as key and the value as the name of the product"
+        product_dict = {}    
+        product_dict['name'] = self.get_product_name(contents) 
+        product_dict['product_id'] = self.get_product_id(contents)
+        product_dict['price'] = self.get_price(contents)
+        product_dict['image'] = self.get_image(contents)
+        product_dict['description'] = self.get_description(contents)
+        return product_dict
+
+    def product_details_to_db(self,product_dict):
+        conn = MySQLdb.connect(host= "localhost",
+                      user="root",
+                      passwd = settings.db_password,
+                      db="jewelryscraperdb")
+        cursor = conn.cursor()
+        cursor.execute (" SELECT * FROM products WHERE product_id = %s ", (product_dict['product_id']))
+        rows = cursor.fetchall()
+        if len(rows) == 0: 
+            cursor.execute (" INSERT INTO products (product_name,product_id,price,image,description) VALUES (%s,%s,%s,%s,%s) "), (product_dict['name'],product_dict['product_id'],product['price'],product['image'], product['description'])
